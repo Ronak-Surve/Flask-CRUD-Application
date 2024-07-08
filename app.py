a407f69d6b783073e8e98383e3d7aa89f23c3068
Ronak-Surve <ronaksurve344rs@gmail.com>
2024-07-08 22:55:46 +0530

    added temporary sessions

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "user123"
+app.permanent_session_lifetime = timedelta(minutes=1)
 
 @app.route("/")
 def index():
@@ -13,17 +16,31 @@ def index():
 def home():
     return "<h1> Home Page </h1>"
 
-@app.route("/<name>")
-def welcome(name):
-    return f"Hello {name}"
-
+@app.route("/user")
+def welcome():
+    if "user" in session:
+        user = session["user"]
+        return f"Hello {user}"
+    else :
+        return redirect(url_for("login"))
+    
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         username = request.form["name"]
-        return redirect(url_for("welcome", name=username))
+        session["user"] = username
+        return redirect(url_for("welcome"))
     else:
+        if "user" in session:
+            return redirect(url_for("welcome"))
         return render_template("login.html")
+    
+
+@app.route("/logout")
+def logout():
+    session.pop("user")
+    return redirect(url_for("login"))
     
 if __name__ in "__main__":
     app.run(debug=True)
